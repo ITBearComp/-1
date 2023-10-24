de150231f965231f1c066e533dd2c3e87432b0ba
ITBearComp <145663029+ITBearComp@users.noreply.github.com>
2023-10-24 12:18:04 +0300

    Update РК1.cpp

--- РК1.cpp
+++ РК1.cpp
@@ -1,7 +1,8 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <cctype>
 #include <algorithm>
+using namespace std;
 
 struct Tovar {
 	std::string name;
@@ -20,8 +21,9 @@ void printTovars(const Tovar* tovars, int size) {         // править
 int main() {
 	setlocale(LC_ALL, "Russian");
 	int numTovar;
-	std::cout << "Введите количество заказанных товаров: ";
+	std::cout << "Введите количество заказанных блюд: ";
 	std::cin >> numTovar;
+	cin.ignore();
 	float SUMM = 0;
 
 	// Выделение памяти для массивов с именами и возрастом студентов
@@ -29,14 +31,15 @@ int main() {
 
 	// Ввод имён и возрастов студентов
 	for (int i = 0; i < numTovar; i++) {
-		std::cout << "Введите название товара " << i + 1 << ": ";
+		std::cout << "Введите название блюда " << i + 1 << ": ";
 		std::cin >> tovars[i].name;
-		std::cout << "Введите количество единиц товара " << i + 1 << ": ";
+		std::cout << "Введите количество порций блюда " << i + 1 << ": ";
 		std::cin >> tovars[i].count;
-		std::cout << "Введите цену за единицу товара " << i + 1 << ": ";
+		std::cout << "Введите стоимость за порцию блюда " << i + 1 << ": ";
 		std::cin >> tovars[i].oneTovar;
 		std::cout << "Общая стоимость заказа: " << tovars[i].oneTovar * tovars[i].count << "\n";
 		SUMM += tovars[i].oneTovar * tovars[i].count;
+		cin.ignore();
 	}
 	// Сортировка студентов по алфавитному порядку
 	std::sort(tovars, tovars + numTovar, [](const Tovar& s1, const Tovar& s2) {
@@ -45,7 +48,7 @@ int main() {
 
 	// Вывод списка студентов
 	printTovars(tovars, numTovar);
-	std::cout << "Общая стоимость заказа :" << SUMM;
+	std::cout << "Общая сумма всех заказов: " << SUMM;
 	/*/ Поиск студента по имени
 	std::string searchName;
 	std::cout << "Введите имя студента для поиска: ";
